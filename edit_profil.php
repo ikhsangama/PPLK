371a2e16c8f8affba6dea0d7fbfec8b44c77d855
ikhsangama <ikhsangama@gmail.com>
2018-10-06 19:27:01 +0700

    edit profil ver1 sukses, validasi password masih error

--- edit_profil.php
+++ edit_profil.php
@@ -15,43 +15,73 @@ $errors = array();
 
 if (Input::get('submit'))
 {
-  if(Input::get('password_baru') ||Input::get('password_lama') || Input::get('password_verify'))
+  // 1. Memanggil obj validasi
+  $validation = new Validation();
+
+  // 2. Metode check
+  $validation = $validation->check(array(
+    'nama' => array(
+      'required' => true,
+      'min' => 3,
+    ),
+    'nama_pemilik' => array(
+      'required' => true,
+      'min' => 3,
+    ),
+  ));
+
+  if($validation->getPassed())
   {
-    // 1. Memanggil obj validasi
-    $validation = new Validation();
-    // 2. Metode check
-    $validation = $validation->check(array(
-      'password_lama' => array(
-        'required' => true,
-      ),
-      'password_baru' => array(
-        'required' => true,
-        'min' => 3,
-      ),
-      'password_verify' => array(
-        'required' => true,
-        'match' => 'password_baru',
-      ),
-    ));
-
-    if($validation->getPassed())
+    // Jika ternyata juga edit password
+    if(Input::get('password_baru') ||Input::get('password_lama') || Input::get('password_verify'))
     {
-      if(password_verify(Input::get('password_lama'), $perusahaan_data['password']))
-      {
-        $perusahaan->update_perusahaan(array(
-          'password' => password_hash(Input::get('password_baru'), PASSWORD_DEFAULT)
-        ), $perusahaan_data['idperusahaan']);
+      // 2. Metode check
+      $validation = $validation->check(array(
+        'password_lama' => array(
+          'required' => true,
+        ),
+        'password_baru' => array(
+          'required' => true,
+          'min' => 3,
+        ),
+        'password_verify' => array(
+          'required' => true,
+          'match' => 'password_baru',
+        ),
+      ));
 
-        Session::flash('profil_baru', 'Data profil anda telah diperbarui');
-        Redirect::to('profil');
+      if($validation->getPassed())
+      {
+        if(password_verify(Input::get('password_lama'), $perusahaan_data['password']))
+        {
+          $perusahaan->update_perusahaan(array(
+            'password' => password_hash(Input::get('password_baru'), PASSWORD_DEFAULT)
+          ), $perusahaan_data['idperusahaan']);
+
+        } else
+        {
+          $errors[] = 'Password lama anda salah';
+        }
       } else
       {
-        $errors[] = 'Password lama anda salah';
+        $errors = $validation -> getErrors(); //harusnya cek error
       }
-    } else
-    {
-      $errors = $validation -> getErrors(); //harusnya cek error
     }
+
+
+    $perusahaan->update_perusahaan(array(
+      'nama' => Input::get('nama'),
+      'nama_pemilik' => Input::get('nama_pemilik'),
+      'alamat' => Input::get('alamat'),
+      'kota' => Input::get('kota'),
+      'no_telp' => Input::get('no_telp'),
+    ), $perusahaan_data['idperusahaan']);
+
+    Session::flash('profil_baru', 'Data profil anda telah diperbarui');
+    Redirect::to('profil');
+  } else
+  {
+    $errors = $validation->getErrors();
   }
 }
 // $perusahaan_data = $perusahaan->get_data(Session::getSession('perusahaan')); dipindah ke header
@@ -77,17 +107,17 @@ require_once "template/header.php"
 
     <h5>Ubah Data</h5>
     <label>Nama: </label>
-    <input type="text" name="nama" value=""><br>
+    <input type="text" name="nama" value=<?php echo $perusahaan_data['nama'] ?>><br>
     <label>Nama Pemilik: </label>
-    <input type="text" name="nama_pemilik" value=""><br>
+    <input type="text" name="nama_pemilik" value=<?php echo $perusahaan_data['nama_pemilik'] ?>><br>
     <label>Alamat: </label>
-    <input type="text" name="alamat" value=""><br>
+    <input type="text" name="alamat" value=<?php echo $perusahaan_data['alamat'] ?>><br>
     <label>Kota: </label>
-    <input type="text" name="kota" value=""><br>
+    <input type="text" name="kota" value=<?php echo $perusahaan_data['kota'] ?>><br>
     <label>Email: </label>
-    <input type="text" name="email" value=""><br>
+    <input type="text" name="kota" value=<?php echo $perusahaan_data['email'] ?> disabled><br>
     <label>No Telp: </label>
-    <input type="text" name="no_telp" value=""><br>
+    <input type="text" name="no_telp" value=<?php echo $perusahaan_data['no_telp'] ?>><br>
 
     <input type="submit" name="submit" value="Daftar Sekarang">
 
